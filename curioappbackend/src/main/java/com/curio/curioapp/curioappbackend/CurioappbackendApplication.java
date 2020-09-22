fb442ade2fa29eee4e908f87fc094150ad8508f4
Kansaji <lakruwan45@gmail.com>
2020-09-22 23:00:40 +0530

    Initialized a spring boot project.updated the application properties file

---
+++ curioappbackend/src/main/java/com/curio/curioapp/curioappbackend/CurioappbackendApplication.java
@@ -0,0 +1,13 @@
+package com.curio.curioapp.curioappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurioappbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurioappbackendApplication.class, args);
+	}
+
+}
---
+++ curioappbackend/src/test/java/com/curio/curioapp/curioappbackend/CurioappbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curio.curioapp.curioappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurioappbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

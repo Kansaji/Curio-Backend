1a71ca355b1d39368449f60be3c54952c95a56b2
Kansaji <lakruwan45@gmail.com>
2020-11-26 11:47:52 +0530

    post item

--- curioappbackend/src/main/java/com/curio/curioapp/curioappbackend/config/SecurityConfig.java
+++ curioappbackend/src/main/java/com/curio/curioapp/curioappbackend/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	public void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/api/auth/**").permitAll().anyRequest().authenticated();
+		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/api/auth/**").permitAll().antMatchers("/api/items/**").permitAll().anyRequest().authenticated();
 		httpSecurity.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 	
